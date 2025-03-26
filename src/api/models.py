6e8a1518620aa9add7055124af85c313db65f92e
kazou92 <99810202+kazou92@users.noreply.github.com>
2025-03-26 21:13:38 +0000

    crud writer ready

--- src/api/models.py
+++ src/api/models.py
@@ -16,4 +16,24 @@ class User(db.Model):
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
-        }
+        } 
+
+
+
+    
+class Writer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80), unique=False, nullable=False)
+    last_name = db.Column(db.String(80), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.Integer, unique=False, nullable=False)
+
+    def __repr__(self):
+        return f'<Writer {self.email}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            # do not serialize the password, its a security breach
+        }    
